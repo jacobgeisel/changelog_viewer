181d673047f99c3c242c39db596e41bc124b77c7
quentin.aubry <aubryquentin700@gmail.com>
2019-12-11 13:27:08 +0100

    [ADD] Adding tree structure of the component and generate component commit-card

---
+++ src/app/feature/commit-card/components/commit-card.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-commit-card',
+  templateUrl: './commit-card.component.html',
+  styleUrls: ['./commit-card.component.css']
+})
+export class CommitCardComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
