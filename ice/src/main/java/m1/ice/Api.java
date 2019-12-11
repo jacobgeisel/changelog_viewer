f3b08a93ee9e9340ca3bbfeeb60af2d1fd34ba09
Mathieu Olives-Delmas <mathod@hotmail.fr>
2019-12-11 11:08:08 +0100

    server

---
+++ ice/src/main/java/m1/ice/Api.java
@@ -0,0 +1,15 @@
+package m1.ice;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class Api {
+
+	@GetMapping(value = "/getCommits/{url}")
+	private String test(){
+	
+		return "TEST OK";
+	}
+	
+}
