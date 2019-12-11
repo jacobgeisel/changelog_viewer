418edc82186319a7b39a14fb3a1b34c1ea6372b9
Mathieu Olives-Delmas <mathod@hotmail.fr>
2019-12-11 14:11:14 +0100

    upload

--- ice/src/main/java/m1/ice/Api.java
+++ ice/src/main/java/m1/ice/Api.java
@@ -1,12 +1,33 @@
 package m1.ice;
 
+import java.io.BufferedOutputStream;
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
 import java.io.UnsupportedEncodingException;
 import java.net.URLDecoder;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.nio.file.StandardCopyOption;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Iterator;
+import java.util.List;
 
+import org.springframework.http.ResponseEntity;
+import org.springframework.util.StringUtils;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
+import org.springframework.web.multipart.MultipartHttpServletRequest;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 @RestController
 public class Api {
@@ -24,5 +45,36 @@ public class Api {
 		return "Url : " + url;
 		
 	}
+	
+	
+	 @PostMapping("/upload") // //new annotation since 4.3
+	    public String singleFileUpload(@RequestParam("file") MultipartFile file,
+	                                   RedirectAttributes redirectAttributes) {
+
+	        if (file.isEmpty()) {
+	            redirectAttributes.addFlashAttribute("message", "Please select a file to upload");
+	            return "redirect:uploadStatus";
+	        }
+
+	        try {
+
+	            // Get the file and save it somewhere
+	            byte[] bytes = file.getBytes();
+	        	System.out.println("okokok");
+
+	            Path path = Paths.get("/Users/mathieu/Desktop/M1ProjectStock/" + file.getOriginalFilename());
+	            Files.write(path, bytes);
+
+	            redirectAttributes.addFlashAttribute("message",
+	                    "You successfully uploaded '" + file.getOriginalFilename() + "'");
+
+	        } catch (IOException e) {
+	            e.printStackTrace();
+	        }
+
+	        return "redirect:/uploadStatus";
+	    }
+	
+	
 	
 }
