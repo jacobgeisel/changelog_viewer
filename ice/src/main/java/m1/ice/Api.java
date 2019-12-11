9d96baa63d7ba98cade496cbc5422d109ce248ff
Mathieu Olives-Delmas <mathod@hotmail.fr>
2019-12-11 11:46:54 +0100

    fix Server

--- ice/src/main/java/m1/ice/Api.java
+++ ice/src/main/java/m1/ice/Api.java
@@ -1,15 +1,27 @@
 package m1.ice;
 
+import java.io.UnsupportedEncodingException;
+import java.net.URLDecoder;
+
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class Api {
 
-	@GetMapping(value = "/getCommits/{url}")
-	private String test(){
+	@GetMapping(value = "/getCommits")
+	private String test(@RequestParam String url){
 	
-		return "TEST OK";
+		try {
+			url = URLDecoder.decode(url, "UTF-8");
+		} catch (UnsupportedEncodingException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+
+		return "Url : " + url;
 		
 	}
 	
